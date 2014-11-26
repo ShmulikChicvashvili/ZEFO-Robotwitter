dedf8744a9647c5eb3d6b85e04780222af0de75e
chicvaXT <shmulikjkech@gmail.com>
2014-11-26 13:07:25 +0200

    Setting the buildCommand in .project file to be a Java project instead of general project. Creating first package and class as an example for usage!

---
+++ src/test/firstClass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Shmulik
+ *
+ */
+public class firstClass
+{	
+	
+	static final int upperBound = 10;
+	/**
+	 * @param args
+	 */
+	public static void main(String [ ] args) {
+		for(int i = 0; i < upperBound; i++) {
+			System.out.println(i);
+		}
+	}
+}

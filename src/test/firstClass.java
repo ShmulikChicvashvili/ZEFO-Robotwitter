746b9dd1d4b4e35456a4aaa5cdc2989e34ae5a0a
Itay <itaykhazon@gmail.com>
2014-11-26 19:59:36 +0200

    Test for the git plugin for eclipse

--- src/test/firstClass.java
+++ src/test/firstClass.java
@@ -1,21 +1,28 @@
 /**
  * 
  */
+
 package test;
+
 
 /**
  * @author Shmulik
  *
  */
 public class firstClass
-{	
+{
 	
-	static final int upperBound = 10;
+	static final int upperBound = 100;
+	
+	
+	
 	/**
 	 * @param args
 	 */
-	public static void main(String [ ] args) {
-		for(int i = 0; i < upperBound; i++) {
+	public static void main(String[] args)
+	{
+		for (int i = 0; i < upperBound; i++)
+		{
 			System.out.println(i);
 		}
 	}

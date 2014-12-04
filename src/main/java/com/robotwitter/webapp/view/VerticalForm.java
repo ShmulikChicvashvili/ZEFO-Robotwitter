27c04aae6fe83170110007ed15c218064a7575e7
Hagai Akibayov <HagaiAkibayov@gmail.com>
2014-12-04 11:49:02 +0200

    Improve VerticalForm.

--- src/main/java/com/robotwitter/webapp/view/VerticalForm.java
+++ src/main/java/com/robotwitter/webapp/view/VerticalForm.java
@@ -23,12 +23,16 @@ import com.vaadin.ui.VerticalLayout;
 
 
 
-/** Vertical form component. */
+/**
+ * Vertical form component.
+ *
+ * @author Hagai Akibayov
+ */
 public class VerticalForm extends CustomComponent
-implements
-Button.ClickListener
+	implements
+		Button.ClickListener
 {
-	
+
 	/**
 	 * Initialise a field.
 	 *
@@ -44,25 +48,26 @@ Button.ClickListener
 		final String caption,
 		final String prompt)
 	{
-		
+
 		// Set caption
 		if (caption != null)
 		{
 			field.setCaption(caption);
 		}
-		
+
 		// Set prompt
 		if (prompt != null)
 		{
 			field.setInputPrompt(prompt);
 		}
-
+		
+		// Set appearance
 		field.setValidationVisible(false);
 		field.setSizeFull();
 		field.setStyleName(FIELD_STYLENAME);
 	}
-
-
+	
+	
 	/**
 	 * Construct a new vertical form component.
 	 *
@@ -73,21 +78,23 @@ Button.ClickListener
 	 */
 	public VerticalForm(final String submitCaption, final FontAwesome submitIcon)
 	{
+		// Construct members
 		fields = new Vector<>();
 		fieldEmptyErrorMessages = new HashMap<>();
 		fieldInvalidErrorMessages = new HashMap<>();
 		fieldValidators = new HashMap<>();
-
+		
+		// Initialise components
 		initialiseErrorMessage();
 		initialiseSubmit(submitCaption, submitIcon);
 		initialiseLayout();
-
-		enableSubmissionOnEnter();
 		
+		enableSubmissionOnEnter();
+
 		setStyleName(FORM_STYLENAME);
 	}
-	
-	
+
+
 	/**
 	 * Add an email address field to the form.
 	 *
@@ -95,24 +102,23 @@ Button.ClickListener
 	 *            The email's caption (or null for none)
 	 * @param prompt
 	 *            The email's prompt (or null for none)
-	 * @param emptyErrorMessage
+	 * @param emptyError
 	 *            The message to display when the email is empty
-	 * @param invalidErrorMessage
+	 * @param invalidError
 	 *            The message to display when the email is invalid
 	 */
 	public void addEmailField(
 		final String caption,
 		final String prompt,
-		final String emptyErrorMessage,
-		final String invalidErrorMessage)
+		final String emptyError,
+		final String invalidError)
 	{
 		final TextField email = new TextField();
-		final EmailValidator validator =
-			new EmailValidator(invalidErrorMessage);
-		addField(email, caption, prompt, emptyErrorMessage, validator);
+		final EmailValidator validator = new EmailValidator(invalidError);
+		addField(email, caption, prompt, emptyError, validator);
 	}
-	
-	
+
+
 	/**
 	 * Add a password field to the form.
 	 *
@@ -134,19 +140,19 @@ Button.ClickListener
 		final PasswordField password = new PasswordField();
 		addField(password, caption, prompt, emptyErrorMessage, validator);
 	}
-	
-	
+
+
 	@Override
 	public void buttonClick(final ClickEvent event)
 	{
 		// Validate
 		if (!validateAllFields()) { return; }
-		
+
 		// Clear any error messages if validation passed successfully
 		clearErrorMessage();
 	}
-	
-	
+
+
 	/**
 	 * @param index
 	 *            The field's index
@@ -158,19 +164,21 @@ Button.ClickListener
 			"Index " + index + " is out of bounds!"); } //$NON-NLS-1$ //$NON-NLS-2$
 		return (AbstractTextField) form.getComponent(index);
 	}
-	
-	
+
+
 	/** Enable submission of the login form upon ENTER key click. */
 	private void enableSubmissionOnEnter()
 	{
+		/** Listener of Enter shortcut keyboard press. */
 		class EnterListener extends Button.ClickShortcut
 		{
+			/** Construct listener on submit button. */
 			public EnterListener()
 			{
 				super(submit, KeyCode.ENTER);
 			}
-			
-			
+
+
 			@Override
 			public void handleAction(final Object sender, final Object target)
 			{
@@ -179,17 +187,17 @@ Button.ClickListener
 					button.click();
 				}
 			}
-			
-			
-			
+
+
+
 			/** Serialisation version unique ID. */
 			private static final long serialVersionUID = 1L;
 		}
-		
+
 		submit.addShortcutListener(new EnterListener());
 	}
-	
-	
+
+
 	/** Initialise the error message displayed upon failed submission. */
 	private void initialiseErrorMessage()
 	{
@@ -197,22 +205,22 @@ Button.ClickListener
 		errorMessage.setVisible(false);
 		errorMessage.setStyleName(ERROR_STYLENAME);
 	}
-	
-	
+
+
 	/** Initialise the form's layout. */
 	private void initialiseLayout()
 	{
 		form = new VerticalLayout();
-
+		
 		fields.forEach(field -> form.addComponent(field));
 		form.addComponent(errorMessage);
 		form.addComponent(submit);
-
+		
 		form.setSpacing(true);
 		setCompositionRoot(form);
 	}
-	
-	
+
+
 	/**
 	 * Initialise the submit button.
 	 *
@@ -233,8 +241,8 @@ Button.ClickListener
 		}
 		submit.setStyleName(SUBMIT_STYLENAME);
 	}
-	
-	
+
+
 	/**
 	 * @param field
 	 *            The field to validate
@@ -248,7 +256,7 @@ Button.ClickListener
 			setErrorMessage(fieldEmptyErrorMessages.get(field), field);
 			return false;
 		}
-		
+
 		// Validate
 		if (!field.isValid())
 		{
@@ -257,11 +265,11 @@ Button.ClickListener
 			errorMessage.setValue(fieldValidators.get(field).getErrorMessage());
 			return false;
 		}
-		
+
 		return true;
 	}
-	
-	
+
+
 	/**
 	 * Add a field to the form.
 	 *
@@ -286,36 +294,36 @@ Button.ClickListener
 		// Initialise field
 		field.addValidator(validator);
 		initialiseField(field, caption, prompt);
-		
+
 		// Add field to internal data structures
 		fields.add(field);
 		fieldEmptyErrorMessages.put(field, emptyErrorMessage);
 		fieldValidators.put(field, validator);
-		
+
 		// Add field to form
 		final int fieldCount = form.getComponentCount() - 2;
 		form.addComponent(field, fieldCount);
-		
+
 		// Focus field if it is the first
 		if (fieldCount == 0)
 		{
 			field.focus();
 		}
 	}
-	
-	
+
+
 	/** Clear any displayed error message on the form. */
 	protected void clearErrorMessage()
 	{
 		errorMessage.setVisible(false);
-		
+
 		fields.forEach(field -> {
 			field.setComponentError(null);
 			field.setValidationVisible(false);
 		});
 	}
-	
-	
+
+
 	/**
 	 * Display an error message on the login form.
 	 *
@@ -330,10 +338,10 @@ Button.ClickListener
 		final AbstractTextField field)
 	{
 		clearErrorMessage();
-		
+
 		errorMessage.setVisible(true);
 		errorMessage.setValue(message);
-		
+
 		if (field != null)
 		{
 			field.setValidationVisible(true);
@@ -341,8 +349,8 @@ Button.ClickListener
 			field.setCursorPosition(field.getValue().length());
 		}
 	}
-	
-	
+
+
 	/**
 	 * Validate all fields in the form
 	 *
@@ -356,43 +364,43 @@ Button.ClickListener
 		}
 		return true;
 	}
-	
-	
-	
+
+
+
 	/** The CSS class name to apply to the form's fields. */
 	private static final String FIELD_STYLENAME = "VerticalForm-field"; //$NON-NLS-1$
-	
+
 	/** The CSS class name to apply to the form's error message. */
 	private static final String ERROR_STYLENAME = "VerticalForm-error"; //$NON-NLS-1$
-	
+
 	/** The CSS class name to apply to the form's submit button. */
 	private static final String SUBMIT_STYLENAME = "VerticalForm-submit"; //$NON-NLS-1$
-	
+
 	/** The CSS class name to apply to the form component. */
 	private static final String FORM_STYLENAME = "VerticalForm"; //$NON-NLS-1$
-
+	
 	/** The form's fields. */
 	List<AbstractTextField> fields;
-
+	
 	/** The error messages displayed when fields are empty. */
 	Map<AbstractTextField, String> fieldEmptyErrorMessages;
-
+	
 	/** The default error messages displayed when fields are invalid. */
 	Map<AbstractTextField, String> fieldInvalidErrorMessages;
-
+	
 	/** Custom validator objects of fields. */
 	Map<AbstractTextField, AbstractStringValidator> fieldValidators;
-	
+
 	/** The submission button. */
 	Button submit;
-	
+
 	/** The error message of a failed submission attempt. */
 	Label errorMessage;
-	
+
 	/** The laid-out form. */
 	private VerticalLayout form;
-	
+
 	/** Serialisation version unique ID. */
 	private static final long serialVersionUID = 1L;
-	
+
 }

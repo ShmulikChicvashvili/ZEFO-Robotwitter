a0b85902e384566b2bb3d987ac89ba6d45264746
AmirDrutin <AmirDrutin@132.68.205.122>
2014-12-31 04:07:45 +0200

    modified follower primitive

--- src/main/java/com/robotwitter/database/primitives/DBFollower.java
+++ src/main/java/com/robotwitter/database/primitives/DBFollower.java
@@ -2,11 +2,17 @@ package com.robotwitter.database.primitives;
 
 import java.sql.Timestamp;
 
+/**
+ * @author Amir and Shmulik
+ */
+
 public class DBFollower {
-	public DBFollower(final String name, final String screenName,
-			final String description, final int followers, final int following,
-			final String location, final int favorites, final String language,
-			final boolean isCelebrity, final Timestamp joined) {
+	public DBFollower(final long followerId, final String name,
+			final String screenName, final String description,
+			final int followers, final int following, final String location,
+			final int favorites, final String language,
+			final boolean isCelebrity, final Timestamp joined, final String picture) {
+		this.followerId = followerId;
 		this.name = name;
 		this.screenName = screenName;
 		this.description = description;
@@ -17,6 +23,7 @@ public class DBFollower {
 		this.language = language;
 		this.isCelebrity = isCelebrity;
 		this.joined = joined;
+		this.picture = picture;
 	}
 
 	@Override
@@ -25,7 +32,7 @@ public class DBFollower {
 			return false;
 		}
 		final DBFollower follower = (DBFollower) obj;
-		return name.equals(follower.name)
+		return followerId == follower.followerId && name.equals(follower.name)
 				&& screenName.equals(follower.screenName)
 				&& description.equals(follower.description)
 				&& followers == follower.followers
@@ -34,7 +41,15 @@ public class DBFollower {
 				&& favorites == follower.favorites
 				&& language.equals(follower.language)
 				&& isCelebrity == follower.isCelebrity
-				&& joined.equals(follower.joined);
+				&& joined.equals(follower.joined)
+				&& picture.equals(follower.picture);
+	}
+
+	/**
+	 * @return the id of the follower
+	 */
+	public long getFollowerId() {
+		return this.followerId;
 	}
 
 	/**
@@ -106,8 +121,14 @@ public class DBFollower {
 	public Timestamp getJoined() {
 		return this.joined;
 	}
-
 	
+	/**
+	 * @return the picture URL of the follower
+	 */
+	public String getPicture() {
+		return this.picture;
+	}
+
 	/**
 	 * @param name
 	 *            the name to set
@@ -188,6 +209,14 @@ public class DBFollower {
 	public void setJoined(Timestamp joined) {
 		this.joined = joined;
 	}
+	
+	/**
+	 * @param picture
+	 *            the picture URL to set
+	 */
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
 
 	/* (non-Javadoc) @see java.lang.Object#toString() */
 	@Override
@@ -197,9 +226,10 @@ public class DBFollower {
 				+ ", following=" + following + ", location=" + location
 				+ ", favorites=" + favorites + ", language=" + language
 				+ ", is the follower a celebrity=" + isCelebrity
-				+ ", joined at=" + joined;
+				+ ", joined at=" + joined + ", picture URL=" + picture +"]";
 	}
 
+	private long followerId;
 	private String name;
 	private String screenName;
 	private String description;
@@ -210,4 +240,5 @@ public class DBFollower {
 	private String language;
 	private boolean isCelebrity;
 	private Timestamp joined;
+	private String picture;
 }

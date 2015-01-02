f48ee0d02fd10b171fdca7847e58767e58501b35
Itay <itaykhazon@gmail.com>
2015-01-02 16:40:52 +0200

    Fixed and implemented a more advanced twitter follower retriever. TODO: test the new class (TwitterFollowerRetriever).

--- src/main/java/com/robotwitter/twitter/ITwitterFollowersRetriever.java
+++ src/main/java/com/robotwitter/twitter/ITwitterFollowersRetriever.java
@@ -5,6 +5,12 @@
 package com.robotwitter.twitter;
 
 
+import java.sql.Timestamp;
+import java.util.TreeMap;
+
+
+
+
 /**
  * @author Itay
  *
@@ -16,10 +22,19 @@ public interface ITwitterFollowersRetriever
 	/**
 	 * @param userID
 	 *            the id of the user we want to retrieve the information about
+	 * @return a TreeMap (hence it is sorted by time) containing the followers
+	 *         count of the user over time.
+	 */
+	TreeMap<Timestamp, Integer> retrieveFollowersAmount(long userID);
+	
+	
+	/**
+	 * @param userID
+	 *            the id of the user we want to retrieve the information about
 	 * @return the number of followers the user has according to this date. If
 	 *         the user doesnt exist, then -1 is returned.
 	 * 
 	 */
-	int retrieveFollowersAmount(long userID);
+	int retrieveLatestFollowersAmount(long userID);
 	
 }

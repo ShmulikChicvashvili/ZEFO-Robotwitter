357ef75ff6d23a44f1db69b1e22bcdb58be63128
chicvaXT <shmulikjkech@gmail.com>
2015-01-04 23:20:26 +0200

    Handled excpetion in integration

--- src/main/java/com/robotwitter/webapp/control/account/TwitterAccountController.java
+++ src/main/java/com/robotwitter/webapp/control/account/TwitterAccountController.java
@@ -1,4 +1,6 @@
+
 package com.robotwitter.webapp.control.account;
+
 
 import java.util.Date;
 import java.util.HashMap;
@@ -8,13 +10,17 @@ import java.util.Map;
 import com.robotwitter.database.interfaces.IDatabaseNumFollowers;
 import com.robotwitter.database.primitives.DBFollowersNumber;
 
+
+
+
 /**
  * A simple implementation of the Twitter account controller interface.
  *
  * @author Doron Hogery
  */
-public class TwitterAccountController implements ITwitterAccountController {
-
+public class TwitterAccountController implements ITwitterAccountController
+{
+	
 	/**
 	 * Instantiates a new twitter account controller.
 	 *
@@ -29,84 +35,113 @@ public class TwitterAccountController implements ITwitterAccountController {
 	 * @param numFollowersDB
 	 *            The number of followers database
 	 */
-	public TwitterAccountController(long id, String name, String screenname,
-			String image, IDatabaseNumFollowers numFollowersDB) {
+	public TwitterAccountController(
+		long id,
+		String name,
+		String screenname,
+		String image,
+		IDatabaseNumFollowers numFollowersDB)
+	{
 		this.id = id;
 		this.name = name;
 		this.screenname = screenname;
 		this.image = image;
 		this.numFollowersDB = numFollowersDB;
 	}
-
+	
+	
 	@Override
-	public final Map<Date, Integer> getAmountOfFollowers(Date from, Date to) {
-
+	public final Map<Date, Integer> getAmountOfFollowers(Date from, Date to)
+	{
+		
 		final Map<Date, Integer> followersBetween = new HashMap<>();
 		final List<DBFollowersNumber> dbfollowers = numFollowersDB.get(id);
-		for (final DBFollowersNumber follower : dbfollowers) {
+		if (dbfollowers == null) { return followersBetween; }
+		for (final DBFollowersNumber follower : dbfollowers)
+		{
 			final Date d = new Date(follower.getDate().getTime());
-			if (from == null && to == null) {
-				followersBetween.put(d,
+			if (from == null && to == null)
+			{
+				followersBetween.put(
+					d,
+					Integer.valueOf(follower.getNumFollowers()));
+			} else if (from == null)
+			{
+				if (follower.getDate().toInstant().compareTo(to.toInstant()) <= 0)
+				{
+					followersBetween.put(
+						d,
 						Integer.valueOf(follower.getNumFollowers()));
-			} else if (from == null) {
-				if (follower.getDate().toInstant().compareTo(to.toInstant()) <= 0) {
-					followersBetween.put(d,
-							Integer.valueOf(follower.getNumFollowers()));
 				}
-			} else if (to == null) {
-				if (follower.getDate().toInstant().compareTo(from.toInstant()) >= 0) {
-					followersBetween.put(d,
-							Integer.valueOf(follower.getNumFollowers()));
+			} else if (to == null)
+			{
+				if (follower.getDate().toInstant().compareTo(from.toInstant()) >= 0)
+				{
+					followersBetween.put(
+						d,
+						Integer.valueOf(follower.getNumFollowers()));
 				}
-			} else {
+			} else
+			{
 				if (follower.getDate().toInstant().compareTo(from.toInstant()) >= 0
-						&& follower.getDate().toInstant()
-								.compareTo(to.toInstant()) <= 0) {
-					followersBetween.put(d,
-							Integer.valueOf(follower.getNumFollowers()));
+					&& follower.getDate().toInstant().compareTo(to.toInstant()) <= 0)
+				{
+					followersBetween.put(
+						d,
+						Integer.valueOf(follower.getNumFollowers()));
 				}
 			}
 		}
 		return followersBetween;
-
+		
 	}
-
+	
+	
 	@Override
-	public final long getID() {
+	public final long getID()
+	{
 		return id;
 	}
-
+	
+	
 	@Override
-	public final String getImage() {
+	public final String getImage()
+	{
 		return image;
 	}
-
+	
+	
 	@Override
-	public final String getName() {
+	public final String getName()
+	{
 		return name;
 	}
-
+	
+	
 	@Override
-	public final String getScreenname() {
+	public final String getScreenname()
+	{
 		return screenname;
 	}
-
+	
+	
+	
 	/** The Twitter accounts' ID. */
 	public long id;
-
+	
 	/** The Twitter accounts' name. */
 	public String name;
-
+	
 	/** The Twitter accounts' screenname. */
 	public String screenname;
-
+	
 	/** The Twitter accounts' profile image. */
 	public String image;
-
+	
 	/** The Twtitter's account number of followers Database */
 	public IDatabaseNumFollowers numFollowersDB;
-
+	
 	/** Serialisation version unique ID. */
 	private static final long serialVersionUID = 1L;
-
+	
 }

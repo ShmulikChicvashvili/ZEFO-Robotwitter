3c2af6d6f9c7be19e9a6dd6edd23956c3df4dfdb
chicvaXT <shmulikjkech@gmail.com>
2015-01-05 02:48:48 +0200

    Final version for submission, finished integration and testing.

--- src/main/java/com/robotwitter/test/TestDatabaseActualFollower.java
+++ src/main/java/com/robotwitter/test/TestDatabaseActualFollower.java
@@ -35,7 +35,7 @@ import com.robotwitter.database.primitives.DBFollower;
 
 public class TestDatabaseActualFollower
 {
-	
+
 	public static final ArrayList<DBFollower> generateFollowersWithName(
 		int amount,
 		String name)
@@ -43,26 +43,22 @@ public class TestDatabaseActualFollower
 		final ArrayList<DBFollower> list = new ArrayList<DBFollower>();
 		for (int i = 0; i < amount; i++)
 		{
-			final DBFollower follower =
-				new DBFollower(
-					i + 100,
-					name,
-					"s" + i,
-					"a",
-					10,
-					10,
-					"l",
-					10,
-					"l",
-					false,
-					Timestamp.valueOf("2005-09-23 10:10:10.0"),
-					"p");
+			final DBFollower follower = new DBFollower(i + 100, name, "s" + i, //$NON-NLS-1$
+				"a", //$NON-NLS-1$
+				10,
+				10,
+				"l", //$NON-NLS-1$
+				10,
+				"l", //$NON-NLS-1$
+				false,
+				Timestamp.valueOf("2005-09-23 10:10:10.0"), //$NON-NLS-1$
+				"p"); //$NON-NLS-1$
 			list.add(follower);
 		}
 		return list;
 	}
-	
-	
+
+
 	public static final ArrayList<DBFollower> generateFollowersWithScreenName(
 		int amount,
 		String screenName)
@@ -70,26 +66,23 @@ public class TestDatabaseActualFollower
 		final ArrayList<DBFollower> list = new ArrayList<DBFollower>();
 		for (int i = 0; i < amount; i++)
 		{
-			final DBFollower follower =
-				new DBFollower(
-					i + 1000,
-					"n" + i,
-					screenName,
-					"a",
-					10,
-					10,
-					"l",
-					10,
-					"l",
-					false,
-					Timestamp.valueOf("2005-09-23 10:10:10.0"),
-					"p");
+			final DBFollower follower = new DBFollower(i + 1000, "n" + i, //$NON-NLS-1$
+				screenName,
+				"a", //$NON-NLS-1$
+				10,
+				10,
+				"l", //$NON-NLS-1$
+				10,
+				"l", //$NON-NLS-1$
+				false,
+				Timestamp.valueOf("2005-09-23 10:10:10.0"), //$NON-NLS-1$
+				"p"); //$NON-NLS-1$
 			list.add(follower);
 		}
 		return list;
 	}
-	
-	
+
+
 	/**
 	 * Before.
 	 */
@@ -98,13 +91,13 @@ public class TestDatabaseActualFollower
 	{
 		final Injector injector =
 			Guice.createInjector(new DatabaseTestModule());
-		
+
 		try (
 			Connection con =
 				injector.getInstance(MySQLConEstablisher.class).getConnection();
 			Statement statement = con.createStatement())
 		{
-			final String dropSchema = "DROP DATABASE `test`";
+			final String dropSchema = "DROP DATABASE `test`"; //$NON-NLS-1$
 			statement.executeUpdate(dropSchema);
 		} catch (final SQLException e)
 		{
@@ -112,50 +105,44 @@ public class TestDatabaseActualFollower
 		}
 		db = injector.getInstance(MySqlDatabaseFollowers.class);
 	}
-	
-	
+
+
 	@Test
 	public final void Test()
 	{
-		final Timestamp joined1 = Timestamp.valueOf("2007-09-23 10:10:10.0");
-		final Timestamp joined2 = Timestamp.valueOf("2008-06-11 12:10:10.0");
-		final DBFollower first =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				666,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				joined1,
-				"twitter.com/random/profile");
+		final Timestamp joined1 = Timestamp.valueOf("2007-09-23 10:10:10.0"); //$NON-NLS-1$
+		final Timestamp joined2 = Timestamp.valueOf("2008-06-11 12:10:10.0"); //$NON-NLS-1$
+		final DBFollower first = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined1,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		if (!db.isExists(123456))
 		{
 			db.insert(first);
 		}
 		assertTrue(db.isExists(1));
-		assertTrue(db.getByName("random").contains(first));
-		assertTrue(db.getByScreenName("randomMan").contains(first));
+		assertTrue(db.getByName("random").contains(first)); //$NON-NLS-1$
+		assertTrue(db.getByScreenName("randomMan").contains(first)); //$NON-NLS-1$
 		assertFalse(db.isExists(9999999));
 		assertFalse(db.isExists(-1));
-		final DBFollower second =
-			new DBFollower(
-				2,
-				"random",
-				"2ndary",
-				"not as handsome and smart as random man",
-				888,
-				555,
-				"Nowhere",
-				2222,
-				"Hebrew",
-				false,
-				joined2,
-				"twitter.com/2ndary/profile");
+		final DBFollower second = new DBFollower(2, "random", //$NON-NLS-1$
+			"2ndary", //$NON-NLS-1$
+			"not as handsome and smart as random man", //$NON-NLS-1$
+			888,
+			555,
+			"Nowhere", //$NON-NLS-1$
+			2222,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined2,
+			"twitter.com/2ndary/profile"); //$NON-NLS-1$
 		if (!db.isExists(222222))
 		{
 			db.insert(second);
@@ -167,41 +154,38 @@ public class TestDatabaseActualFollower
 		}
 		assertTrue(db.isExists(1, 2));
 	}
-	
-	
+
+
 	@Test
 	public final void testGetsUpdateDelete()
 	{
-		final DBFollower mainUser =
-			new DBFollower(
-				123456789,
-				"man",
-				"manG",
-				"some man",
-				9999,
-				9999,
-				"somewhere",
-				9999,
-				"English",
-				false,
-				Timestamp.valueOf("2010-10-10 10:10:10.0"),
-				"twitter.com/man/profile");
+		final DBFollower mainUser = new DBFollower(123456789, "man", //$NON-NLS-1$
+			"manG", //$NON-NLS-1$
+			"some man", //$NON-NLS-1$
+			9999,
+			9999,
+			"somewhere", //$NON-NLS-1$
+			9999,
+			"English", //$NON-NLS-1$
+			false,
+			Timestamp.valueOf("2010-10-10 10:10:10.0"), //$NON-NLS-1$
+			"twitter.com/man/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.SUCCESS, db.insert(mainUser));
-		
+
 		final ArrayList<DBFollower> nameFollowers =
-			generateFollowersWithName(100, "Yossi");
+			generateFollowersWithName(100, "Yossi"); //$NON-NLS-1$
 		final ArrayList<DBFollower> screenNameFollowers =
-			generateFollowersWithScreenName(100, "theYossi");
+			generateFollowersWithScreenName(100, "theYossi"); //$NON-NLS-1$
 		final ArrayList<DBFollower> updatedFollowers =
-			generateFollowersWithName(100, "Menahem");
+			generateFollowersWithName(100, "Menahem"); //$NON-NLS-1$
 		ArrayList<Long> followersById = db.getFollowersId(123456789);
 		assertTrue(followersById.size() == 0);
-		
+
 		assertEquals(SqlError.INVALID_PARAMS, db.deleteFollower(-1));
 		assertEquals(SqlError.INVALID_PARAMS, db.deleteFollow(-1, -1));
 		assertEquals(SqlError.INVALID_PARAMS, db.deleteFollow(-1, 99));
 		assertEquals(SqlError.INVALID_PARAMS, db.deleteFollow(99, -1));
-		
+
 		for (final DBFollower follower : nameFollowers)
 		{
 			assertEquals(SqlError.DOES_NOT_EXIST, db.update(follower));
@@ -209,7 +193,7 @@ public class TestDatabaseActualFollower
 				SqlError.DOES_NOT_EXIST,
 				db.deleteFollower(follower.getFollowerId()));
 		}
-		
+
 		for (final DBFollower follower : nameFollowers)
 		{
 			assertFalse(db.isExists(follower.getFollowerId()));
@@ -218,12 +202,12 @@ public class TestDatabaseActualFollower
 				SqlError.SUCCESS,
 				db.insert(123456789, follower.getFollowerId()));
 		}
-		
-		final ArrayList<DBFollower> followerNames = db.getByName("Yossi");
+
+		final ArrayList<DBFollower> followerNames = db.getByName("Yossi"); //$NON-NLS-1$
 		assertTrue(followerNames.size() == 100);
 		followersById = db.getFollowersId(123456789);
 		assertTrue(followersById.size() == 100);
-		
+
 		for (final DBFollower follower : screenNameFollowers)
 		{
 			assertFalse(db.isExists(follower.getFollowerId()));
@@ -232,23 +216,23 @@ public class TestDatabaseActualFollower
 				SqlError.SUCCESS,
 				db.insert(123456789, follower.getFollowerId()));
 		}
-		
+
 		final ArrayList<DBFollower> followerScreenNames =
-			db.getByScreenName("theYossi");
+			db.getByScreenName("theYossi"); //$NON-NLS-1$
 		assertTrue(followerScreenNames.size() == 100);
 		followersById = db.getFollowersId(123456789);
 		assertTrue(followersById.size() == 200);
-		
+
 		for (final DBFollower follower : updatedFollowers)
 		{
 			assertEquals(SqlError.SUCCESS, db.update(follower));
 		}
 		final ArrayList<DBFollower> previousNameFollowers =
-			db.getByName("Yossi");
+			db.getByName("Yossi"); //$NON-NLS-1$
 		assertTrue(previousNameFollowers.size() == 0);
-		ArrayList<DBFollower> updatedNameFollowers = db.getByName("Menahem");
+		ArrayList<DBFollower> updatedNameFollowers = db.getByName("Menahem"); //$NON-NLS-1$
 		assertTrue(updatedNameFollowers.size() == 100);
-		
+
 		for (final DBFollower follower : updatedFollowers)
 		{
 			assertEquals(
@@ -260,17 +244,17 @@ public class TestDatabaseActualFollower
 				db.deleteFollower(follower.getFollowerId()));
 			assertFalse(db.isExists(follower.getFollowerId()));
 		}
-		
-		updatedNameFollowers = db.getByName("Menahem");
+
+		updatedNameFollowers = db.getByName("Menahem"); //$NON-NLS-1$
 		assertTrue(updatedNameFollowers.size() == 0);
 	}
-	
-	
+
+
 	@Test
 	public final void testInsertGet()
 	{
-		final Timestamp joined = Timestamp.valueOf("2005-09-23 10:10:10.0");
-		final Timestamp joined2 = Timestamp.valueOf("2007-11-11 11:10:10.0");
+		final Timestamp joined = Timestamp.valueOf("2005-09-23 10:10:10.0"); //$NON-NLS-1$
+		final Timestamp joined2 = Timestamp.valueOf("2007-11-11 11:10:10.0"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(null));
 		DBFollower badFollower =
 			new DBFollower(
@@ -287,219 +271,179 @@ public class TestDatabaseActualFollower
 				null,
 				null);
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				-1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				666,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(-1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				null,
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				666,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, null, "randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				null,
-				"handsome and smart random man",
-				999,
-				666,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			null,
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				null,
-				999,
-				666,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			null,
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				-1,
-				666,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			-1,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				-1,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			-1,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				666,
-				null,
-				6969,
-				"Hebrew",
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			null,
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				666,
-				"Everywhere",
-				-1,
-				"Hebrew",
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			-1,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				666,
-				"Everywhere",
-				6969,
-				null,
-				false,
-				joined,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			null,
+			false,
+			joined,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				666,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				null,
-				"twitter.com/random/profile");
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			null,
+			"twitter.com/random/profile"); //$NON-NLS-1$
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		badFollower =
-			new DBFollower(
-				1,
-				"random",
-				"randomMan",
-				"handsome and smart random man",
-				999,
-				666,
-				"Everywhere",
-				6969,
-				"Hebrew",
-				false,
-				joined,
-				null);
+		badFollower = new DBFollower(1, "random", //$NON-NLS-1$
+			"randomMan", //$NON-NLS-1$
+			"handsome and smart random man", //$NON-NLS-1$
+			999,
+			666,
+			"Everywhere", //$NON-NLS-1$
+			6969,
+			"Hebrew", //$NON-NLS-1$
+			false,
+			joined,
+			null);
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(badFollower));
-		
+
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(-1, -1));
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(567, -1));
 		assertEquals(SqlError.INVALID_PARAMS, db.insert(-1, 222));
-		
+
 		assertEquals(null, db.get(-1));
 		assertEquals(null, db.get(666));
-		final DBFollower okFollower =
-			new DBFollower(
-				666,
-				"man",
-				"manG",
-				"some man",
-				12,
-				23,
-				"somewhere",
-				2,
-				"English",
-				false,
-				joined,
-				"twitter.com/man/profile");
-		final DBFollower secondFollower =
-			new DBFollower(
-				999,
-				"2ndman",
-				"2manG",
-				"2nd man",
-				55,
-				66,
-				"2nd place",
-				1,
-				"Chinese",
-				false,
-				joined2,
-				"twitter.com/man/profile");
+		final DBFollower okFollower = new DBFollower(666, "man", //$NON-NLS-1$
+			"manG", //$NON-NLS-1$
+			"some man", //$NON-NLS-1$
+			12,
+			23,
+			"somewhere", //$NON-NLS-1$
+			2,
+			"English", //$NON-NLS-1$
+			false,
+			joined,
+			"twitter.com/man/profile"); //$NON-NLS-1$
+		final DBFollower secondFollower = new DBFollower(999, "2ndman", //$NON-NLS-1$
+			"2manG", //$NON-NLS-1$
+			"2nd man", //$NON-NLS-1$
+			55,
+			66,
+			"2nd place", //$NON-NLS-1$
+			1,
+			"Chinese", //$NON-NLS-1$
+			false,
+			joined2,
+			"twitter.com/man/profile"); //$NON-NLS-1$
 		assertFalse(db.getFollowersId(666).contains(secondFollower));
 		assertTrue(db.getFollowersId(666).isEmpty());
-		
+
 		assertEquals(SqlError.SUCCESS, db.insert(okFollower));
 		assertEquals(SqlError.ALREADY_EXIST, db.insert(okFollower));
 		assertEquals(SqlError.SUCCESS, db.insert(secondFollower));
-		
+
 		assertEquals(SqlError.SUCCESS, db.insert(666, 999));
 		assertEquals(SqlError.ALREADY_EXIST, db.insert(666, 999));
 	}
-	
-	
-	
+
+
+
 	/** The Database */
 	IDatabaseFollowers db;
 }

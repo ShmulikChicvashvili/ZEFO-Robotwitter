0c06c0bb31861316fa92619202dce82c6f7cd2ee
Hagai Akibayov <HagaiAkibayov@gmail.com>
2015-01-05 18:39:45 +0200

    Add header to analysis page.

--- src/main/java/com/robotwitter/webapp/view/analysis/AnalysisView.java
+++ src/main/java/com/robotwitter/webapp/view/analysis/AnalysisView.java
@@ -24,7 +24,9 @@ import org.dussan.vaadin.dcharts.renderers.tick.CanvasAxisTickRenderer;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 import com.robotwitter.webapp.control.account.ITwitterAccountController;
 import com.robotwitter.webapp.messages.IMessagesContainer;
@@ -41,7 +43,7 @@ import com.robotwitter.webapp.view.AbstractView;
  */
 public class AnalysisView extends AbstractView
 {
-	
+
 	/**
 	 * Instantiates a new login view.
 	 *
@@ -52,54 +54,54 @@ public class AnalysisView extends AbstractView
 	public AnalysisView(@Named(NAME) IMessagesContainer messages)
 	{
 		super(messages, messages.get("AnalysisView.page.title"));
-
+		
 		chart = null;
 	}
-
-
+	
+	
 	@Override
 	public final void activateTwitterAccount(long id)
 	{
 		updateChart();
 	}
-
-
+	
+	
 	@Override
 	public final boolean isSignedInProhibited()
 	{
 		return false;
 	}
-	
-	
+
+
 	@Override
 	public final boolean isSignedInRequired()
 	{
 		return true;
 	}
-	
-	
+
+
 	/** Update the followers over time chart. */
 	private void updateChart()
 	{
 		ITwitterAccountController controller =
 			getUserSession().getAccountController().getActiveTwitterAccount();
-		
+
 		Map<Date, Integer> followers =
 			controller.getAmountOfFollowers(null, null);
-		
+
 		DataSeries dataSeries = new DataSeries().newSeries();
-		
+
 		DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-		
+
 		for (Map.Entry<Date, Integer> entry : followers.entrySet())
 		{
 			dataSeries.add(df.format(entry.getKey()), entry.getValue());
 		}
-		
+
 		chart.setDataSeries(dataSeries).show();
 	}
-
-
+	
+	
 	@Override
 	protected final void initialise()
 	{
@@ -108,53 +110,62 @@ public class AnalysisView extends AbstractView
 				new XYaxis().setRenderer(AxisRenderers.DATE).setTickOptions(
 					new CanvasAxisTickRenderer().setAngle(-30).setFormatString(
 						"%#d %b, %Y"))).addAxis(
-				new XYaxis(XYaxes.Y)
-					.setLabel(
-						messages.get("AnalysisView.chart.label.followers"))
-					.setLabelRenderer(LabelRenderers.CANVAS)
-					.setTickOptions(
-						new AxisTickRenderer().setFormatString("%d")));
-		
+							new XYaxis(XYaxes.Y)
+							.setLabel(
+								messages.get("AnalysisView.chart.label.followers"))
+								.setLabelRenderer(LabelRenderers.CANVAS)
+								.setTickOptions(
+									new AxisTickRenderer().setFormatString("%d")));
+
 		Highlighter highlighter =
 			new Highlighter()
-				.setShow(true)
-				.setSizeAdjust(10)
-				.setTooltipLocation(TooltipLocations.NORTH)
-				.setTooltipAxes(TooltipAxes.YX)
-				.setUseAxesFormatters(true)
-				.setFormatString("%s followers on %s");
-		
+		.setShow(true)
+		.setSizeAdjust(10)
+		.setTooltipLocation(TooltipLocations.NORTH)
+		.setTooltipAxes(TooltipAxes.YX)
+		.setUseAxesFormatters(true)
+		.setFormatString("%s followers on %s");
+
 		Options options =
 			new Options()
-		.addOption(axes)
-		.addOption(highlighter)
-		.setAnimate(true);
-		
+				.addOption(axes)
+				.addOption(highlighter)
+				.setAnimate(true);
+
 		chart = new DCharts().setOptions(options);
 		updateChart();
-		
-		setCompositionRoot(chart);
-		
+
 		UI.getCurrent().getPage().addBrowserWindowResizeListener(event -> {
 			chart.setSizeFull();
 		});
-
-		addStyleName(STYLENAME);
 		
+		addStyleName(STYLENAME);
+
+		Label header = new Label(messages.get("AnalysisView.label.header"));
+		header.addStyleName(HEADER_STYLENAME);
+		VerticalLayout layout = new VerticalLayout(header, chart);
+		layout.setSizeFull();
+		layout.setExpandRatio(chart, 1);
+
+		setCompositionRoot(layout);
+
 		getUserSession().observeActiveTwitterAccount(this);
 	}
-	
-	
-	
+
+
+
 	/** The view's name. */
 	public static final String NAME = "analysis";
-	
+
 	/** The CSS class name to apply to this component. */
 	private static final String STYLENAME = "AnalysisView";
 
+	/** The CSS class name to apply to this component. */
+	private static final String HEADER_STYLENAME = "AnalysisView-header";
+	
 	/** The followers over time chart. */
 	DCharts chart;
-
+	
 	/** Serialisation version unique ID. */
 	private static final long serialVersionUID = 1L;
 }

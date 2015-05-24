76350eec99d9a7c12d9bc90470c81d73b5c295e8
chicvaXT <shmulikjkech@gmail.com>
2015-05-25 01:18:43 +0300

    Created the primitive which holds the scheduled tweet details

---
+++ src/main/java/com/robotwitter/database/primitives/DBScheduledTweet.java
@@ -0,0 +1,66 @@
+package com.robotwitter.database.primitives;
+
+import java.util.Calendar;
+
+import com.robotwitter.posting.AutomateTweetPostingPeriod;
+import com.robotwitter.twitter.TwitterAccount;
+
+public class DBScheduledTweet extends DatabaseType {
+
+	public DBScheduledTweet(String email, TwitterAccount twitterAccount,
+			String tweetName, String tweetText, Calendar startingDate,
+			AutomateTweetPostingPeriod postingPeriod) {
+		super(email);
+		this.twitterAccount = twitterAccount;
+		this.tweetName = tweetName;
+		this.tweetText = tweetText;
+		this.startingDate = startingDate;
+		this.postingPeriod = postingPeriod;
+	}
+
+	public TwitterAccount getTwitterAccount() {
+		return twitterAccount;
+	}
+
+	public void setTwitterAccount(TwitterAccount twitterAccount) {
+		this.twitterAccount = twitterAccount;
+	}
+
+	public String getTweetName() {
+		return tweetName;
+	}
+
+	public void setTweetName(String tweetName) {
+		this.tweetName = tweetName;
+	}
+
+	public String getTweetText() {
+		return tweetText;
+	}
+
+	public void setTweetText(String tweetText) {
+		this.tweetText = tweetText;
+	}
+
+	public Calendar getStartingDate() {
+		return startingDate;
+	}
+
+	public void setStartingDate(Calendar startingDate) {
+		this.startingDate = startingDate;
+	}
+
+	public AutomateTweetPostingPeriod getPostingPeriod() {
+		return postingPeriod;
+	}
+
+	public void setPostingPeriod(AutomateTweetPostingPeriod postingPeriod) {
+		this.postingPeriod = postingPeriod;
+	}
+
+	TwitterAccount twitterAccount;
+	String tweetName;
+	String tweetText;
+	Calendar startingDate;
+	AutomateTweetPostingPeriod postingPeriod;
+}
